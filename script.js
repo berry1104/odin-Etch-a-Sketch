0969cf3c330fd7cad474e4c499e87fffd2057122
berry1104 <935311076@qq.com>
2024-02-03 14:04:24 +0800

    change size in a variable and fix cell's size

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 // create a 16x16 grid of square divs
 // Use flexbox to make the divs appear as a grid (versus just one on each line)
 const container = document.querySelector('#grid-container');
+let gridSize = 55;
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.classList.add('grid-item');
-    container.appendChild(div);
+function createGrid(numberOfSquares) {
+    container.style.setProperty('--items-per-row', numberOfSquares);
+
+    // Clear previous cells if any
+    container.innerHTML = '';
+
+    // Create cells
+    for (let i = 0; i < numberOfSquares * numberOfSquares; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        // Add additional cell setup here if needed
+        container.appendChild(cell);
+    }
 }
 
+
+createGrid(gridSize);
+
+
 // Add a hover effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through the grid like a pen
-const gridItems = document.querySelectorAll('.grid-item');
-gridItems.forEach(item => {
-    item.addEventListener('mouseover', () => {
-        item.style.backgroundColor = 'pink';
+
+const cells = document.querySelectorAll('.cell');
+cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+        cell.style.backgroundColor = 'pink';
     });
 });
